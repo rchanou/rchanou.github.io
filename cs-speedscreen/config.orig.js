7846a4b3b688f20c0aa507f627ded6e27a9a21ec
brianchuchua <bchuchua@gmail.com>
2014-07-15 16:27:38 -0700

    Updated cs-speedscreen to no longer mandate https

--- cs-speedscreen/config.orig.js
+++ cs-speedscreen/config.orig.js
@@ -1,5 +1,5 @@
 var config =
 {
-    apiURL: 'https://' + window.location.hostname + '/api/index.php',
+    apiURL: 'http://' + window.location.hostname + '/api/index.php',
     apiKey: 'cs-dev'
 };
