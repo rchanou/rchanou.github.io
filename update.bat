b39ef2f5a934be556ed12260d4b087318e008cb6
brianchuchua <bchuchua@gmail.com>
2014-07-30 16:34:46 -0700

    Step 1 of truly reverting web.config changes
    All web.configs will be stored as backups, and then restored from those
    backups after a git pull.
    
    Second phase: Once this script is out everywhere, the web.configs will
    be removed from the Git project. This should allow their .gitignores to
    kick in, and they'll be restored afterwards and never touched.

--- update.bat
+++ update.bat
@@ -1,4 +1,15 @@
 REM git fetch origin master
 REM git reset --hard HEAD
 REM git clean -df
+
+copy "admin\www\web.config" "admin\www\web.config.bak" /Y
+copy "api\web.config" "api\web.config.bak" /Y
+copy "booking\www\web.config" "booking\www\web.config.bak" /Y
+copy "cs-registration\www\web.config" "cs-registration\www\web.config.bak" /Y
+
 git pull https://clubspeed-api-updater:,git2014!GusGus,@github.com/clubspeed/clubspeedapps.git
+
+copy "admin\www\web.config.bak" "admin\www\web.config" /Y
+copy "api\web.config.bak" "api\web.config" /Y
+copy "booking\www\web.config.bak" "booking\www\web.config" /Y
+copy "cs-registration\www\web.config.bak" "cs-registration\www\web.config" /Y
