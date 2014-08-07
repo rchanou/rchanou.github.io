0463404212a3bbeb2b862dbf3ec7634872dbcc44
Wes Ratcliff <wes.ratcliff@gmail.com>
2014-08-07 15:59:25 -0700

    Adding a hook to speed up slow Last Updated query

--- api/Racers.php
+++ api/Racers.php
@@ -19,9 +19,11 @@ require_once('vendors/swiftmailer/lib/swift_required.php');
 class Racers
 {
     public $restler;
+    private $db;
 
 	function __construct(){
 		header('Access-Control-Allow-Origin: *'); //Here for all /say
+        $this->db = $GLOBALS['db'];
 	}
 
 	private function getNextCustomerId() {
@@ -792,27 +794,29 @@ EOD;
 
 		$racers = $this->run_query($tsql, $tsql_params);
 
-		foreach($racers as $id => $racer) {
-			
-			// Add memberships
-			$racers[$id]['memberships'] = array();
-			$memberships = $this->run_query("GetCustomerMemberships {$racer['custid']}", array());
-			foreach($memberships as $membership) {
-				$racers[$id]['memberships'][] = array('name' => $membership['Description'], 'expiration' => $membership['ExpirationDate']);
+		if(!isset($_GET['suppress_additional_fields'])) { // Hook to prevent these "heavy" queries from being executed
+			foreach($racers as $id => $racer) {
+				
+				// Add memberships
+				$racers[$id]['memberships'] = array();
+				$memberships = $this->run_query("GetCustomerMemberships {$racer['custid']}", array());
+				foreach($memberships as $membership) {
+					$racers[$id]['memberships'][] = array('name' => $membership['Description'], 'expiration' => $membership['ExpirationDate']);
+				}
+				
+				// Add points
+				$points = $this->run_query("GetCustomerStandardPoints {$racer['custid']}", array());
+				$racers[$id]['points'] = floatval($points[0]['Points']);
+				
+				// Add cell phone consent and date
+				$racers[$id]['cell_consent_given'] = empty($racer['cell']) ? false : true;
+				$racers[$id]['cell_consent_date']  = empty($racers[$id]['cell_consent_given']) ? null : $racer['accountcreated'];
+				
+				// Add email consent and date
+				$racers[$id]['email_consent_given'] = ($racer['donotmail'] == 1) ? false : true;
+				$racers[$id]['email_consent_date']  = empty($racers[$id]['email_consent_given']) ? null : $racer['accountcreated'];
+				
 			}
-			
-			// Add points
-			$points = $this->run_query("GetCustomerStandardPoints {$racer['custid']}", array());
-			$racers[$id]['points'] = floatval($points[0]['Points']);
-			
-			// Add cell phone consent and date
-			$racers[$id]['cell_consent_given'] = empty($racer['cell']) ? false : true;
-			$racers[$id]['cell_consent_date']  = empty($racers[$id]['cell_consent_given']) ? null : $racer['accountcreated'];
-			
-			// Add email consent and date
-			$racers[$id]['email_consent_given'] = ($racer['donotmail'] == 1) ? false : true;
-			$racers[$id]['email_consent_date']  = empty($racers[$id]['email_consent_given']) ? null : $racer['accountcreated'];
-			
 		}
 
 		return array('racers' => $racers);
