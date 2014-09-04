9ee48f465ac95a3fb0832e1c46fe440b91700e58
Wes Ratcliff <wes.ratcliff@gmail.com>
2014-09-04 09:48:01 -0700

    Adding migration for admin panel

---
+++ api/migrations/201409040900.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This script installs the config.php for the new admin panel using
+ * the private key from the API.
+ */
+
+$path_to_admin_config_directory = '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR . 'laravel' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
+
+// See if we need to rename config.php
+if(file_exists($path_to_admin_config_directory . 'config.php')) {
+    die($path_to_admin_config_directory . 'config.php already exists. Exiting!');
+}
+
+// Get private key
+require_once('../config.php');
+if(!isset($privateKey)) {
+	die('$privateKey is not set! Exiting!');
+}
+
+// Get configuration template
+$configuration_template = file_get_contents($path_to_admin_config_directory . 'config.orig.php');
+
+// Replace INSERT_PRIVATE_KEY_HERE with $privateKey in configuration template
+$configuration_template = str_replace('INSERT_PRIVATE_KEY_HERE', $privateKey, $configuration_template);
+
+// Write configuration template
+file_put_contents($path_to_admin_config_directory . 'config.php', $configuration_template);
+
+// Confirm success
+die('Successfully wrote admin configuration');
