cb24fed2ab1f0c07e2d7e69445ec71a0fee52492
Wes Ratcliff <wes.ratcliff@gmail.com>
2014-10-01 11:37:35 -0700

    Added settings for Reset Password template

---
+++ api/migrations/201410011133 - Reset Password Settings.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Create the settings (with defaults) for "Reset Password" email if they do
+ * not already exist.
+ */
+
+$settings = array(
+	'resetPasswordHTML' => '<h1>Reset Password Email HTML</h1>',
+	'resetPasswordTEXT' => 'Reset Password Email TEXT',
+	);
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+$conn = new PDO( "sqlsrv:server=(local) ; Database=ClubSpeedV8", "", "");
+$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+$stmt = $conn->prepare("INSERT INTO ControlPanel (TerminalName, SettingName, DataType, DefaultSetting, SettingValue, Description, Fixed, CreatedDate) VALUES (:TerminalName, :SettingName, :DataType, :DefaultSetting, :SettingValue, '', :Fixed, GETDATE())");
+
+foreach($settings as $SettingName => $SettingValue) {
+	$TerminalName = 'Templates';
+	$Fixed = false;
+	$DefaultSetting = $SettingValue;
+	$DataType = gettype($SettingValue) === 'boolean' ? 'bit' : 65535;
+	
+	$sth = $conn->prepare("SELECT * FROM dbo.ControlPanel WHERE TerminalName = :TerminalName AND SettingName = :SettingName");
+	$sth->bindParam(':TerminalName', $TerminalName);
+	$sth->bindParam(':SettingName', $SettingName);
+	$sth->execute();
+	$existingEntry = $sth->fetchAll();
+	
+	// If it doesn't exist, insert it
+	if(count($existingEntry) === 0) {
+		$stmt->bindParam(':TerminalName', $TerminalName);
+		$stmt->bindParam(':SettingName', $SettingName);
+		$stmt->bindParam(':SettingValue', $SettingValue);
+		$stmt->bindParam(':DefaultSetting', $DefaultSetting);
+		$stmt->bindParam(':DataType', $DataType);
+		$stmt->bindParam(':Fixed', $Fixed);
+		$stmt->execute();
+		echo "Inserting " . $SettingName . "<br/>";
+	} else {
+		echo "Did not create " . $SettingName . " (already exists)<br/>";
+	}
+
+}
+
+// Confirm success
+die('Successfully imported online booking settings.');
