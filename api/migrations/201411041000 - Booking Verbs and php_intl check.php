3d165eae3b1c03f9fac89640121ff4bc807dc44e
Wes Ratcliff <wes.ratcliff@gmail.com>
2014-11-04 11:54:29 -0800

    Adding handler for Windows 2003

--- api/migrations/201411041000 - Booking Verbs and php_intl check.php
+++ api/migrations/201411041000 - Booking Verbs and php_intl check.php
@@ -14,15 +14,25 @@ if(!extension_loaded('intl')) {
 
 echo '<hr/>';
 
-// Load web.config and check for proper HTTP verbs inside of it
-$path_to_api_webDotConfig = '..' . DIRECTORY_SEPARATOR . 'web.config';
-$webDotConfig = file_get_contents($path_to_api_webDotConfig);
-$occurrences = strpos($webDotConfig, 'php" verb="GET,HEAD,POST,PUT,DELETE"');
+$server_version = php_uname();
 
-if(!$occurrences){
-	echo '<h1 style="color: red;">HTTP Verbs Missing! Action Required!</h1>';
-	echo '<p>Please edit "C:\ClubSpeedApps\api\web.config" on the line starting with: <em>&lt;add name="php-5.3.28"...</em> (php version may differ slightly)</p>';
-	echo '<p>Replace <em>verb="GET,HEAD,POST"</em> to read: <strong>verb="GET,HEAD,POST,PUT,DELETE"</strong></p>';
+if(strpos($server_version, 'Windows Server 2003')) {
+
+	echo '<p>This appears to be a Windows 2003 Server -- ALL HTTP Verbs should be enabled by default for .php in the /api project.';
+
 } else {
-	echo '<p style="color: green;">PHP verbs for booking web.config are already in place, no further action is necessary.</p>';
+
+	// Load web.config and check for proper HTTP verbs inside of it
+	$path_to_api_webDotConfig = '..' . DIRECTORY_SEPARATOR . 'web.config';
+	$webDotConfig = file_get_contents($path_to_api_webDotConfig);
+	$occurrences = strpos($webDotConfig, 'php" verb="GET,HEAD,POST,PUT,DELETE"');
+	
+	if(!$occurrences){
+		echo '<h1 style="color: red;">HTTP Verbs Missing! Action Required!</h1>';
+		echo '<p>Please edit "C:\ClubSpeedApps\api\web.config" on the line starting with: <em>&lt;add name="php-5.3.28"...</em> (php version may differ slightly)</p>';
+		echo '<p>Replace <em>verb="GET,HEAD,POST"</em> to read: <strong>verb="GET,HEAD,POST,PUT,DELETE"</strong></p>';
+	} else {
+		echo '<p style="color: green;">PHP verbs for booking web.config are already in place, no further action is necessary.</p>';
+	}
+
 }
