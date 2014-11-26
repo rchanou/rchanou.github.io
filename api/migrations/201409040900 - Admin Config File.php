32c5ed78445e8d975cf6d591028634d1264a1a48
Wes Ratcliff <wes.ratcliff@gmail.com>
2014-11-26 12:49:20 -0800

    Handing private key not set in config file

--- api/migrations/201409040900 - Admin Config File.php
+++ api/migrations/201409040900 - Admin Config File.php
@@ -6,17 +6,29 @@
 
 $path_to_admin_config_directory = '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR . 'laravel' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
 
-// See if we need to rename config.php
+// See if we need to run checks on an existing config.php
 $admin_config_file = $path_to_admin_config_directory . 'config.php';
 if(file_exists($admin_config_file)) {
-    // If it exists but is 0 bytes, we should delete it
-		if(filesize( $admin_config_file) == 0) {
+
+    // If file has INSERT_PRIVATE_KEY_HERE, delete it
+		if(strpos(file_get_contents($admin_config_file), 'INSERT_PRIVATE_KEY_HERE')) {
+
+			echo 'Config file does not have private key set -- removing and replacing with config.orig.php<br/>';
+			unlink($admin_config_file); // It exists but is not setup, delete it and remake
+			copy($path_to_admin_config_directory . 'config.orig.php', $admin_config_files);
+
+		} elseif(filesize($admin_config_file) == 0) { // It exists but is 0 bytes, we should delete it
+
 			echo 'Config file is 0 bytes -- removing and replacing with config.orig.php<br/>';
 			unlink($admin_config_file);
 			copy($path_to_admin_config_directory . 'config.orig.php', $admin_config_files);
-		} else {
+
+		} else { // File exists and has been edited
+
 			die($path_to_admin_config_directory . 'config.php already exists. Exiting!');
+
 		}
+
 }
 
 // Get private key
