0689e9c2385bf947aad55744e89f427d3d73e157
Wes Ratcliff <wes.ratcliff@gmail.com>
2015-02-18 10:48:22 -0800

    Updating Admin Footer Links

--- admin/laravel/app/views/footer.blade.php
+++ admin/laravel/app/views/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
     <div id="footer" class="col-xs-12">
-        &copy; <a href="http://www.clubspeedtiming.com" target="_blank">Club Speed, Inc.</a> 2006-{{date("Y")}} - <a href="http://clubspeedtiming.com/Contact.php" target="_blank">Contact Us</a>
+        &copy; <a href="http://www.clubspeed.com" target="_blank">Club Speed, Inc.</a> 2006-{{date("Y")}} - <a href="http://www.clubspeed.com/contact/" target="_blank">Contact Us</a>
     </div>
 </div>
