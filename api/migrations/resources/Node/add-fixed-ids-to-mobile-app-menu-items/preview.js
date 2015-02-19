401de3a655ea2a69a162574004781d4c232b223f
Ronald Chanou <rchanou@gmail.com>
2015-02-19 15:52:20 -0800

    removes node migration since it doesn't work on other servers

--- api/migrations/resources/Node/add-fixed-ids-to-mobile-app-menu-items/preview.js
+++
@@ -1,9 +0,0 @@
-console.log('running preview');
-
-var Promise = require('bluebird');
-var constructAsync = Promise.promisify(require('./construct'));
-
-constructAsync()
-.then(function(res){
-  console.log(res);
-});
