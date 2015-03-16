03c220586b4e7666bc184899c79ca0760f994372
brianchuchua <bchuchua@gmail.com>
2015-03-16 12:07:06 -0700

    Added migration for new gift card templates
    Migration has been edited to temporarily overwrite our older gift card
    template. (Safe as no customer has used or customized it yet.) Will be
    switched to non-overwriting migration once all tracks have been
    migrated.

--- api/migrations/201501021500 - Gift Card Templates.php
+++ api/migrations/201501021500 - Gift Card Templates.php
@@ -5,6 +5,7 @@ require_once('../ClubSpeed/ClubSpeedLoader.php');
 $_REQUEST['debug'] = true;
 
 $giftCardHtml = file_get_contents(__DIR__.'/resources/201501021500 - HTML01 - gift card.html');
+$giftCardHtml = str_replace('##TRACKURL##',$_SERVER['HTTP_HOST'],$giftCardHtml);
 
 $settings = array(
     array(
@@ -31,13 +32,16 @@ foreach($settings as $setting) {
             'Namespace' => $setting['Namespace']
             , 'Name'    => $setting['Name']
         ));
-        if (empty($existing)) {
+        if (empty($existing))
+        {
             $db->settings->create($setting);
             echo 'Setting (' . $setting['Namespace'] . ', ' . $setting['Name']  . ') successfully imported!';
             echo '<br>';
         }
         else {
-            echo 'Setting (' . $setting['Namespace'] . ', ' . $setting['Name'] . ') already exists!';
+            $setting['SettingsID'] = $existing[0]->SettingsID;
+            $db->settings->update($setting);
+            echo 'Setting (' . $setting['Namespace'] . ', ' . $setting['Name'] . ') already exists! FORCING AN UPDATE instead.';
             echo '<br>';
         }
     }
@@ -46,4 +50,24 @@ foreach($settings as $setting) {
         echo '<br>';
     }
 }
+
+// COPY DEFAULT IMAGES TO ASSETS
+$src = 'resources/templates/giftcardemails/';
+$dst = '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . 'giftcardemails' . DIRECTORY_SEPARATOR;
+$files = glob("resources/templates/giftcardemails/*.*");
+
+// Ensure the directory exists, if not, create it!
+if(!is_dir($dst)) mkdir($dst, null, true);
+
+// Copy each file
+foreach($files as $file){
+    $file_to_go = str_replace($src, $dst, $file);
+    copy($file, $file_to_go);
+}
+
+// Fix permissions on Windows (works on 2003?). This is because by default the uploaded imaged
+// does not inherit permissions from the folder it is moved to. Instead, it retains the
+// permissions of the temporary folder.
+exec('c:\windows\system32\icacls.exe ' . $dst . ' /inheritance:e');
+
 die('Successfully imported receipt email templates.');
