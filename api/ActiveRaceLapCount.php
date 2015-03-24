eb7a098cd5b6f0ffbd917588a10b626ae126d665
Dave Lesage <dlesage27@gmail.com>
2015-03-24 09:41:41 -0700

    /api - hiding unnecessary ActiveRaceLapCount calls, making others public for scoreboard usage

--- api/ActiveRaceLapCount.php
+++ api/ActiveRaceLapCount.php
@@ -1,11 +1,17 @@
 <?php
 
-use ClubSpeed\Database\Helpers\UnitOfWork;
+use Clubspeed\Enums\Enums;
 
 class ActiveRaceLapCount extends BaseUowApi {
 
     function __construct() {
         parent::__construct();
         $this->resource = 'activeRaceLapCount';
+
+        $this->access['all']        = Enums::API_PUBLIC_ACCESS;
+        $this->access['get']        = Enums::API_PUBLIC_ACCESS;
+        $this->access['delete']     = Enums::API_NO_ACCESS;
+        $this->access['create']     = Enums::API_NO_ACCESS;
+        $this->access['put']        = Enums::API_NO_ACCESS;
     }
 }
