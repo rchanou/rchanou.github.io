ad707ae19c04183169124e2ccf9e4067a20986a2
brianchuchua <bchuchua@gmail.com>
2015-05-04 10:08:41 -0700

    Fixed assets folder being ignored

---
+++ cs-speedscreen/js/assets/settings.js
@@ -0,0 +1,8 @@
+var settings = {
+      "defaultLocale": "en-US",
+      "apiDriver": "polling",
+      "channelUpdateFrequencyMs": 60000,
+      "racesPollingRateMs": 3000,
+      "timeUntilRestartOnErrorMs": 30000,
+      "channelSource": "old"
+};
