455a14217ac9ee028044ac517cd9176c1d409072
Dave Lesage <dlesage27@gmail.com>
2015-08-21 09:46:26 -0700

    /api - feat: make amb gets public

--- api/AMB.php
+++ api/AMB.php
@@ -1,9 +1,13 @@
 <?php
 
+use ClubSpeed\Enums\Enums as Enums;
+
 class AMB extends BaseUowApi {
 
     function __construct() {
         parent::__construct();
         $this->resource = 'AMB';
+        $this->access['all'] = Enums::API_PUBLIC_ACCESS;
+        $this->access['get'] = Enums::API_PUBLIC_ACCESS;
     }
 }
