b27f00ccc39c4e73bf0716fc11ae413a85b39d55
wesratcliff <wes.ratcliff@gmail.com>
2015-10-12 09:22:02 -0700

    doc: Documenting aclKeys config.php paramter

--- api/config.orig.php
+++ api/config.orig.php
@@ -10,6 +10,34 @@ $authentication_keys = array($privateKey, 'cs-dev', md5(date('Y-m-d')));
 $apiUsername = 'PUT_USERNAME_HERE';
 $apiPassword = 'PUT_PASSWORD_HERE';
 
+/**
+ * FINE GRAINED WHITELIST ACCESS CONTROL OVER API ENDPOINTS
+ *
+ * This access control is a whitelist and uses PHP Regex grant access to the requested HTTP_METHOD:URL.
+ * "public" is a reserved "regex" to give access to any non-private resource.
+ *
+ * HOW THE REQUESTED URL IS CREATED AND MATCHED
+ *  $requestedApiEndpoint = $_SERVER['REQUEST_METHOD'] . ':' . strtolower($_SERVER['PATH_INFO']);
+ *  $foundMatch = preg_match($regex, $requestedApiEndpoint, $matches);
+ *  If "$foundMatch" is true, the request is allowed.
+ *  Regexes are processed in array order from 0 .. N
+ *
+ * EXAMPLE "$requestedApiEndpoint"s
+ * - GET:/customers/1000001.json
+ * - GET:/customers
+ * - DELETE:/customers
+ *
+ * EXAMPLE REGEXS -- Test/Create with: http://www.phpliveregex.com/ */
+ // "Private, all access": '/.*/' (this is the equivalent of a "$privateKey")
+ // "All Public": "public" (this is a special keyword)
+ // "READ Customers": "/GET:\/customers\/.*/"
+ // "READ/CREATE Customers": "/[GET|POST]+:\/customers\/.*/"
+ // "READ a Specific Customer": "/GET+:\/customers\/1000001.*/"
+
+$aclKeys = array(
+	// 'privateKeyHere' => array('regex1Here', 'regex2Here'),
+);
+
 // URL to images used for the digital signage aka "Speed Screen"
 $channelImageUrl = '/sp_admin/ScreenImages/'; // This is hardcoded in Club Speed, cannot get elsewhere. :-(
 
