rchanou/rchanou.github.io

commit 1abbf345fb31157cffbacfc7699795537ad689ae

/api - fix: prevent a duplicate welcome email with racers/register

api/Racers.php
@@ -216,14 +216,30 @@ class Racers
             if ($this->is_fb_registration($params)) {
                 // facebook registration
                 $customer = $this->postfb_login($request_data);
+                $this->run_new_account_logic($request_data, $params, $customer['customerId']);
             }
             else {
                 // standard registration
                 $customer = $this->postCreate($request_data);
+                $customerId = $customer['customerId'];
+                $params['customerId'] = $customerId;
+
+                // dont run all new account logic, just the queue part of it
+                if ($this->is_event_registration($params)) {
+                    $this->getapplyRule(
+                        $customerId
+                        , Enums::RULE_ADD_EVENT_CUSTOMER_FROM_ONLINE_REGISTRATION
+                    );
+                }
+                else {
+                    $this->getapplyRule(
+                        $customerId
+                        , Enums::RULE_ADD_CUSTOMER_FROM_REGISTRATION_TERMINAL
+                    );
+                }
             }
 
             // send new account emails and create waivers
-            $this->run_new_account_logic($request_data, $params, $customer['customerId']);
 
             // add to event or customer queue
             

commit 1b61f246212b6c2aee271f15efd48355a3540257

/api - fix: allow waiver/sig images to be created for non FB-registrations
Fixes issue introduced by commit
1abbf345fb31157cffbacfc7699795537ad689ae

api/Racers.php
@@ -174,8 +174,7 @@ class Racers
                 , 'Token'           => $token
             ));
             Authenticate::impersonate($token); // impersonate customer for the remaining calls
-            if (isset($request_data['profilephoto']))
-                $this->profile_photo($customerId, $request_data);
+            $this->create_photos($request_data, $customerId);
             if (isset($request_data['email']))
                 $this->welcome($customerId, $request_data);
             return array(